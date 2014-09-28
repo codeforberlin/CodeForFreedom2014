0b06f36369128028503842baadf5ee96b6173f49
Tobias Preuss <tobias.preuss@googlemail.com>
2014-09-28 11:32:42 +0200

    Prepare country map with initial people.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Code for Freedom
+
+![Code for Freedom Logo](gfx/codeforfreedom-logo.png "Code for Freedom Logo")
+
+This map shows where all the smart people come from who attended the [Code for Freedom](http://www.codeforfreedom.org) event.
--- js/main.js
+++ js/main.js
@@ -14,4 +14,39 @@ require(["leaflet"], function(L){
   L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
     attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
   }).addTo(map);
+
+
+  // Selected countries
+  var countries;
+  $.getJSON("countries/codeforfreedom-countries.geojson", function(data) {
+    countries = data;
+  })
+  .done(function(){
+    L.geoJson(countries, { style: {
+      fillColor: "#d8c92d",
+      color: "#4cba6a"
+    }}).addTo(map);
+  });
+
+
+  // People from everywhere
+  var people;
+  $.getJSON("people/people.json", function(data) {
+    people = data;
+  })
+  .done(function(){
+    add_people(people);
+  });
+
+  var mappedPeople = [];
+  function add_people(data){
+    map.removeLayer(mappedPeople);
+    mappedPeople = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        // TODO: Render popup
+      }
+    }).addTo(map);
+  }
+
+
 });
